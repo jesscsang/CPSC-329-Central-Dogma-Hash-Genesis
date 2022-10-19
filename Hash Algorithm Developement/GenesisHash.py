b868032e3e057f3d490abb2a59add71ec1f62077
jesscsang <71296352+jesscsang@users.noreply.github.com>
2022-10-19 15:11:42 -0600

    got the same length functionality down

--- Hash Algorithm Developement/GenesisHash.py
+++ Hash Algorithm Developement/GenesisHash.py
@@ -39,54 +39,103 @@ def remapASCII(ASCII):
     return floor(floor(((L_0+3)**10 + (L_1+3)**10)/3) /
                  (L_2+3))**3 % (10**14)
 
-
-def append_even_string_index(str):
-    result = ""
-    for i in range(len(str)):
-        if i % 2 == 0:
-            result += str[i]
-    return int(result)
-
-
-def sum_odd_string_index(str):
-    result = 0
-    for i in range(len(str)):
-        if i % 2 != 0:
-            result += int(str[i])
-    return result
-
-def appendEvenOrOddIndices(str, type="even"):
-    result = ""
-    for i in range(len(str)):
-        if (i % 2 == 0 and type == "even"):
-            result += str[i]
-        if (i % 2 != 0 and type != "even"):
-            result += str[i]
-    return int(result)
-
 def strToRemappedASCII(string):
     resultASCII = ""
     for char in string:
         resultASCII += str(remapASCII(ord(char)))
     return resultASCII
 
+def appendEvenOrOddIndices(str, type="even",length=-1):
+    result = ""
+    i = 0
+    counter = 0
+    lengthReached = False
+    while (i < len(str) and not lengthReached):
+        if (i % 2 == 0 and type == "even"):
+            result += str[i]
+            counter += 1
+        if (i % 2 != 0 and type != "even"):
+            result += str[i]
+            counter += 1
+        i+=1
+        if (length != -1 and counter > length ):
+            lengthReached = True
+    return int(result)
+
+def decimalToBin(decimal):
+    strDecimal = str(decimal)
+    binary = ""
+    for i in range(len(strDecimal)):
+        binary += str(int(strDecimal[i]) % 2)
+    return binary
+
+def binToRNA(bin):
+    DNA_Result = ""
+    bin2DnaMap = {"00": "A", "01": "U", "10": "G", "11": "C"}
+
+    for i in range(0, len(bin)-1, 2):
+        binaryPair = bin[i]+bin[i+1]
+
+        for key in bin2DnaMap:
+            if (key == binaryPair):
+                DNA_Result += bin2DnaMap[key]
+
+    return DNA_Result
+
+def RNA2aminoAcids(RNA):
+    aaResult = ""
+    RNA_Codons = {
+        # 'M' - START, '_' - STOP
+        "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+        "UGU": "C", "UGC": "C",
+        "GAU": "D", "GAC": "D",
+        "GAA": "E", "GAG": "E",
+        "UUU": "F", "UUC": "F",
+        "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+        "CAU": "H", "CAC": "H",
+        "AUA": "I", "AUU": "I", "AUC": "I",
+        "AAA": "K", "AAG": "K",
+        "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+        "AUG": "M",
+        "AAU": "N", "AAC": "N",
+        "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+        "CAA": "Q", "CAG": "Q",
+        "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+        "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+        "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+        "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+        "UGG": "W",
+        "UAU": "Y", "UAC": "Y",
+        "UAA": "_", "UAG": "_", "UGA": "_"
+    }
+
+    for i in range(0, len(RNA)-2, 3):
+        rnaCodon = RNA[i]+RNA[i+1]+RNA[i+2]
+        for key in RNA_Codons:
+            if (key == rnaCodon):
+                aaResult += RNA_Codons[key]
+    return aaResult
 
 password = "password"
 
 remappedASCII_PW = strToRemappedASCII(password)
-print(f'Remapped ASCII: {remappedASCII_PW}')
-print(f'Even: {appendEvenOrOddIndices(remappedASCII_PW,"even")}')
-print(f'Odd: {appendEvenOrOddIndices(remappedASCII_PW,"odd")}')
+# print(f'Remapped ASCII: {remappedASCII_PW}')
+# print(f'Even: {appendEvenOrOddIndices(remappedASCII_PW,"even")}')
+# print(f'Odd: {appendEvenOrOddIndices(remappedASCII_PW,"odd")}')
 _hash = appendEvenOrOddIndices(
     remappedASCII_PW,"even") + appendEvenOrOddIndices(remappedASCII_PW,"odd")
 
-print(f'sum: {_hash}\n')
 
-while _hash < 10**512:
+while _hash < 10**(256*2*3+2*256):  # double desire length plus buffer
     _hash = _hash**3
 
-_hash = appendEvenOrOddIndices(str(_hash),"odd")
+_hash = appendEvenOrOddIndices(str(_hash),"odd",256*2*3)
 
+binary = decimalToBin(_hash)
 
-print(f'hash: {_hash}\n')
-print(len(str(_hash)))
+RNA = binToRNA(binary)
+
+AA = RNA2aminoAcids(RNA)
+
+print(f'hash: {AA}\n')
+print(len(AA))
